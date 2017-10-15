215bfe1fc0451bc116a6fe6aea228aa0d1f5bc24
Lee Griffiths <griffter@hotmail.co.uk>
2017-10-15 23:05:04 +0100

    Removed "Conjured Mana Cake" from golden master test (they were not part of the original strategy)

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -36,15 +36,15 @@ namespace GildedRose.Tests
             inventory.AddItem(ItemBuilder.AnItem().WithName("Elixir of the Mongoose").WithSellIn(5).WithQuality(7).Build());
             inventory.AddItem(ItemBuilder.AnItem().WithName("Sulfuras, Hand of Ragnaros").WithSellIn(0).WithQuality(80).Build());
             inventory.AddItem(ItemBuilder.AnItem().WithName("Backstage passes to a TAFKAL80ETC concert").WithSellIn(15).WithQuality(20).Build());
-            inventory.AddItem(ItemBuilder.AnItem().WithName("Conjured Mana Cake").WithSellIn(3).WithQuality(6).Build());
-
+          
             return inventory;   
         }
 
         public void InitialiseGoldenMaster()
-        { 
+        {
             goldenMasterInventories.Add(CreateInitialInventory());
-            for (int iterations = 0; iterations < 10; iterations++) {
+            for (int iterations = 0; iterations < 10; iterations++)
+            {
                 goldenMasterInventories.Add(Inventory.Create(ItemUpdateStrategyFactory.Instance));
             }
 
@@ -53,71 +53,61 @@ namespace GildedRose.Tests
             goldenMasterInventories[1].AddItem(ItemBuilder.AnItem().WithName("Elixir of the Mongoose").WithSellIn(4).WithQuality(6).Build());
             goldenMasterInventories[1].AddItem(ItemBuilder.AnItem().WithName("Sulfuras, Hand of Ragnaros").WithSellIn(0).WithQuality(80).Build());
             goldenMasterInventories[1].AddItem(ItemBuilder.AnItem().WithName("Backstage passes to a TAFKAL80ETC concert").WithSellIn(14).WithQuality(21).Build());
-            goldenMasterInventories[1].AddItem(ItemBuilder.AnItem().WithName("Conjured Mana Cake").WithSellIn(2).WithQuality(4).Build());
 
             goldenMasterInventories[2].AddItem(ItemBuilder.AnItem().WithName("+5 Dexterity Vest").WithSellIn(8).WithQuality(18).Build());
             goldenMasterInventories[2].AddItem(ItemBuilder.AnItem().WithName("Aged Brie").WithSellIn(0).WithQuality(2).Build());
             goldenMasterInventories[2].AddItem(ItemBuilder.AnItem().WithName("Elixir of the Mongoose").WithSellIn(3).WithQuality(5).Build());
             goldenMasterInventories[2].AddItem(ItemBuilder.AnItem().WithName("Sulfuras, Hand of Ragnaros").WithSellIn(0).WithQuality(80).Build());
             goldenMasterInventories[2].AddItem(ItemBuilder.AnItem().WithName("Backstage passes to a TAFKAL80ETC concert").WithSellIn(13).WithQuality(22).Build());
-            goldenMasterInventories[2].AddItem(ItemBuilder.AnItem().WithName("Conjured Mana Cake").WithSellIn(1).WithQuality(2).Build());
 
             goldenMasterInventories[3].AddItem(ItemBuilder.AnItem().WithName("+5 Dexterity Vest").WithSellIn(7).WithQuality(17).Build());
             goldenMasterInventories[3].AddItem(ItemBuilder.AnItem().WithName("Aged Brie").WithSellIn(-1).WithQuality(4).Build());
             goldenMasterInventories[3].AddItem(ItemBuilder.AnItem().WithName("Elixir of the Mongoose").WithSellIn(2).WithQuality(4).Build());
             goldenMasterInventories[3].AddItem(ItemBuilder.AnItem().WithName("Sulfuras, Hand of Ragnaros").WithSellIn(0).WithQuality(80).Build());
             goldenMasterInventories[3].AddItem(ItemBuilder.AnItem().WithName("Backstage passes to a TAFKAL80ETC concert").WithSellIn(12).WithQuality(23).Build());
-            goldenMasterInventories[3].AddItem(ItemBuilder.AnItem().WithName("Conjured Mana Cake").WithSellIn(0).WithQuality(0).Build());
 
             goldenMasterInventories[4].AddItem(ItemBuilder.AnItem().WithName("+5 Dexterity Vest").WithSellIn(6).WithQuality(16).Build());
             goldenMasterInventories[4].AddItem(ItemBuilder.AnItem().WithName("Aged Brie").WithSellIn(-2).WithQuality(6).Build());
             goldenMasterInventories[4].AddItem(ItemBuilder.AnItem().WithName("Elixir of the Mongoose").WithSellIn(1).WithQuality(3).Build());
             goldenMasterInventories[4].AddItem(ItemBuilder.AnItem().WithName("Sulfuras, Hand of Ragnaros").WithSellIn(0).WithQuality(80).Build());
             goldenMasterInventories[4].AddItem(ItemBuilder.AnItem().WithName("Backstage passes to a TAFKAL80ETC concert").WithSellIn(11).WithQuality(24).Build());
-            goldenMasterInventories[4].AddItem(ItemBuilder.AnItem().WithName("Conjured Mana Cake").WithSellIn(-1).WithQuality(0).Build());
 
             goldenMasterInventories[5].AddItem(ItemBuilder.AnItem().WithName("+5 Dexterity Vest").WithSellIn(5).WithQuality(15).Build());
             goldenMasterInventories[5].AddItem(ItemBuilder.AnItem().WithName("Aged Brie").WithSellIn(-3).WithQuality(8).Build());
             goldenMasterInventories[5].AddItem(ItemBuilder.AnItem().WithName("Elixir of the Mongoose").WithSellIn(0).WithQuality(2).Build());
             goldenMasterInventories[5].AddItem(ItemBuilder.AnItem().WithName("Sulfuras, Hand of Ragnaros").WithSellIn(0).WithQuality(80).Build());
             goldenMasterInventories[5].AddItem(ItemBuilder.AnItem().WithName("Backstage passes to a TAFKAL80ETC concert").WithSellIn(10).WithQuality(25).Build());
-            goldenMasterInventories[5].AddItem(ItemBuilder.AnItem().WithName("Conjured Mana Cake").WithSellIn(-2).WithQuality(0).Build());
 
             goldenMasterInventories[6].AddItem(ItemBuilder.AnItem().WithName("+5 Dexterity Vest").WithSellIn(4).WithQuality(14).Build());
             goldenMasterInventories[6].AddItem(ItemBuilder.AnItem().WithName("Aged Brie").WithSellIn(-4).WithQuality(10).Build());
             goldenMasterInventories[6].AddItem(ItemBuilder.AnItem().WithName("Elixir of the Mongoose").WithSellIn(-1).WithQuality(0).Build());
             goldenMasterInventories[6].AddItem(ItemBuilder.AnItem().WithName("Sulfuras, Hand of Ragnaros").WithSellIn(0).WithQuality(80).Build());
             goldenMasterInventories[6].AddItem(ItemBuilder.AnItem().WithName("Backstage passes to a TAFKAL80ETC concert").WithSellIn(9).WithQuality(27).Build());
-            goldenMasterInventories[6].AddItem(ItemBuilder.AnItem().WithName("Conjured Mana Cake").WithSellIn(-3).WithQuality(0).Build());
 
             goldenMasterInventories[7].AddItem(ItemBuilder.AnItem().WithName("+5 Dexterity Vest").WithSellIn(3).WithQuality(13).Build());
             goldenMasterInventories[7].AddItem(ItemBuilder.AnItem().WithName("Aged Brie").WithSellIn(-5).WithQuality(12).Build());
             goldenMasterInventories[7].AddItem(ItemBuilder.AnItem().WithName("Elixir of the Mongoose").WithSellIn(-2).WithQuality(0).Build());
             goldenMasterInventories[7].AddItem(ItemBuilder.AnItem().WithName("Sulfuras, Hand of Ragnaros").WithSellIn(0).WithQuality(80).Build());
             goldenMasterInventories[7].AddItem(ItemBuilder.AnItem().WithName("Backstage passes to a TAFKAL80ETC concert").WithSellIn(8).WithQuality(29).Build());
-            goldenMasterInventories[7].AddItem(ItemBuilder.AnItem().WithName("Conjured Mana Cake").WithSellIn(-4).WithQuality(0).Build());
 
             goldenMasterInventories[8].AddItem(ItemBuilder.AnItem().WithName("+5 Dexterity Vest").WithSellIn(2).WithQuality(12).Build());
             goldenMasterInventories[8].AddItem(ItemBuilder.AnItem().WithName("Aged Brie").WithSellIn(-6).WithQuality(14).Build());
             goldenMasterInventories[8].AddItem(ItemBuilder.AnItem().WithName("Elixir of the Mongoose").WithSellIn(-3).WithQuality(0).Build());
             goldenMasterInventories[8].AddItem(ItemBuilder.AnItem().WithName("Sulfuras, Hand of Ragnaros").WithSellIn(0).WithQuality(80).Build());
             goldenMasterInventories[8].AddItem(ItemBuilder.AnItem().WithName("Backstage passes to a TAFKAL80ETC concert").WithSellIn(7).WithQuality(31).Build());
-            goldenMasterInventories[8].AddItem(ItemBuilder.AnItem().WithName("Conjured Mana Cake").WithSellIn(-5).WithQuality(0).Build());
 
             goldenMasterInventories[9].AddItem(ItemBuilder.AnItem().WithName("+5 Dexterity Vest").WithSellIn(1).WithQuality(11).Build());
             goldenMasterInventories[9].AddItem(ItemBuilder.AnItem().WithName("Aged Brie").WithSellIn(-7).WithQuality(16).Build());
             goldenMasterInventories[9].AddItem(ItemBuilder.AnItem().WithName("Elixir of the Mongoose").WithSellIn(-4).WithQuality(0).Build());
             goldenMasterInventories[9].AddItem(ItemBuilder.AnItem().WithName("Sulfuras, Hand of Ragnaros").WithSellIn(0).WithQuality(80).Build());
             goldenMasterInventories[9].AddItem(ItemBuilder.AnItem().WithName("Backstage passes to a TAFKAL80ETC concert").WithSellIn(6).WithQuality(33).Build());
-            goldenMasterInventories[9].AddItem(ItemBuilder.AnItem().WithName("Conjured Mana Cake").WithSellIn(-6).WithQuality(0).Build());
 
             goldenMasterInventories[10].AddItem(ItemBuilder.AnItem().WithName("+5 Dexterity Vest").WithSellIn(0).WithQuality(10).Build());
             goldenMasterInventories[10].AddItem(ItemBuilder.AnItem().WithName("Aged Brie").WithSellIn(-8).WithQuality(18).Build());
             goldenMasterInventories[10].AddItem(ItemBuilder.AnItem().WithName("Elixir of the Mongoose").WithSellIn(-5).WithQuality(0).Build());
             goldenMasterInventories[10].AddItem(ItemBuilder.AnItem().WithName("Sulfuras, Hand of Ragnaros").WithSellIn(0).WithQuality(80).Build());
             goldenMasterInventories[10].AddItem(ItemBuilder.AnItem().WithName("Backstage passes to a TAFKAL80ETC concert").WithSellIn(5).WithQuality(35).Build());
-            goldenMasterInventories[10].AddItem(ItemBuilder.AnItem().WithName("Conjured Mana Cake").WithSellIn(-7).WithQuality(0).Build());
-        }   
+        }
 
 
         [Fact]
