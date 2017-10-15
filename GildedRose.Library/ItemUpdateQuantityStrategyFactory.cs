eba4297228fab898508aeca3885daf55437e9c2c
Lee Griffiths <griffter@hotmail.co.uk>
2017-10-15 20:44:25 +0100

    Created an Item Update Quantity strategy for Aged Brie

--- GildedRose.Library/ItemUpdateQuantityStrategyFactory.cs
+++ GildedRose.Library/ItemUpdateQuantityStrategyFactory.cs
@@ -10,16 +10,51 @@ namespace GildedRose
     {
         public IItemUpdateQuantityStrategy StrategyFor(string itemName)
         {
-            return DefaultItemUpdateStrategy.Create();
+            if (itemName == "Aged Brie")
+            {
+                return AgedBrieItemUpdateStrategy.Create();
+            }
+            else
+            {
+                return DefaultItemUpdateStrategy.Create();
+            }
+        }
+    }
+
+
+    //All items have a SellIn value which denotes the number of days we have to sell the item
+    //All items have a Quality value which denotes how valuable the item is
+    //At the end of each day our system lowers both values for every item
+    //Pretty simple, right? Well this is where it gets interesting:
+
+    //Once the sell by date has passed, Quality degrades twice as fast
+    //The Quality of an item is never negative
+
+    //"Aged Brie" actually increases in Quality the older it gets
+    //The Quality of an item is never more than 50
+    //"Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+    //"Backstage passes", like aged brie, increases in Quality as it's SellIn value approaches; Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
+
+    class AgedBrieItemUpdateStrategy : IItemUpdateQuantityStrategy
+    {
+        private AgedBrieItemUpdateStrategy() { }
+
+        public static IItemUpdateQuantityStrategy Create()
+        {
+            return new AgedBrieItemUpdateStrategy();
+        }
+
+        public void UpdateQuantity(Item item)
+        {
+            item.SellIn--;
+            item.Quality = item.Quality == 50 ? 50 :
+                           item.Quality + (item.SellIn < 0 ? 2 : 1);
         }
     }
 
     class DefaultItemUpdateStrategy : IItemUpdateQuantityStrategy
     {
-        private DefaultItemUpdateStrategy()
-        {
-
-        }
+        private DefaultItemUpdateStrategy() { }
 
         public static IItemUpdateQuantityStrategy Create()
         {
