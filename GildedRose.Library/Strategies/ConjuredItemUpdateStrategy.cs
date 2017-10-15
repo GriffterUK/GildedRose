8f68989221a552bb51942b062c4420dc4beeba77
Lee Griffiths <griffter@hotmail.co.uk>
2017-10-15 22:36:39 +0100

    Added Update Quantity strategy for "Conjured" items

---
+++ GildedRose.Library/Strategies/ConjuredItemUpdateStrategy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRose.Strategies
+{
+    public class ConjuredItemUpdateStrategy : IItemUpdateStrategy
+    {
+        private ConjuredItemUpdateStrategy() { }
+
+        public static IItemUpdateStrategy Create()
+        {
+            return new ConjuredItemUpdateStrategy();
+        }
+
+        public void Update(Item item)
+        {
+            ItemModifier.Modify(item)
+                .IncreaseAge()
+                .DecreaseQuality()
+                .DecreaseQuality()
+                .LimitQualityToNoLessThan(0);
+        }
+    }
+}
