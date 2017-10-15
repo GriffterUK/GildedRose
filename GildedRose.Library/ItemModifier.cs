b5fef4177d37e9b75ef69788fa3f7c950e9b24d9
Lee Griffiths <griffter@hotmail.co.uk>
2017-10-15 21:43:02 +0100

    Refactored the default strategy to use item modifier (and confirmed all strategies match with Golden Master)

--- GildedRose.Library/ItemModifier.cs
+++ GildedRose.Library/ItemModifier.cs
@@ -32,9 +32,21 @@ namespace GildedRose
             return this;
         }
 
+        public ItemModifier DecreaseQuality()
+        {
+            this.item.Quality--;
+            return this;
+        }
+
         public ItemModifier IncreaseQualityWhenExpiryInLessThan(int sellInLessThan)
         {
             this.item.Quality+=this.item.SellIn < sellInLessThan ? 1 : 0 ;
+            return this;
+        }
+
+        public ItemModifier DecreaseQualityWhenExpired()
+        {
+            this.item.Quality -= this.item.SellIn < 0 ? 1 : 0;
             return this;
         }
 
@@ -50,13 +62,18 @@ namespace GildedRose
             return this;
         }
 
-        public ItemModifier LimitQualityTo(int qualityLimit)
+        public ItemModifier LimitQualityToNoMoreThan(int qualityLimit)
         {
             this.item.Quality = Math.Min(qualityLimit, this.item.Quality);
             return this;
         }
 
-            
+        public ItemModifier LimitQualityToNoLessThan(int qualityLimit)
+        {
+            this.item.Quality = Math.Max(qualityLimit, this.item.Quality);
+            return this;
+        }
 
+       
     }
 }
